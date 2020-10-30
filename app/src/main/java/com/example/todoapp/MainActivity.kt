ff37cc53de62daefcdf5cc325dbdb43ce015f75d
william <williamlopes64@gmail.com>
2020-10-30 19:28:00 -0300

    Adding custom_input.xml, ic_add, ic_no_data.xml, arrays.xml and list_fragment_menu.xml; designing the fragment's layouts.

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setNavigationSettings()
     }
+
+    private fun setNavigationSettings() {
+        //setSupportActionBar(toolbar_activity)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        //val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.listFragment)
+        )
+
+        setupActionBar(navController, appBarConfiguration)
+    }
+
+    private fun setupActionBar(navController:NavController, appBarConfig:AppBarConfiguration){
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.navHostFragment).navigateUp(appBarConfiguration)
+
 }
--- app/src/main/java/com/example/todoapp/fragment/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/fragment/add/AddFragment.kt
@@ -15,7 +15,8 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        return view
     }
 
 }
