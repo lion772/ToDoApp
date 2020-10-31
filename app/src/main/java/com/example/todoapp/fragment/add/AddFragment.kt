48b3a592a96d916638bc1525a3f1b97a8dda8869
william <williamlopes64@gmail.com>
2020-10-31 00:05:08 -0300

    Creating animation during the transition of fragments: from_left.xml, from_right.xml, to_left.xml, to_right.xml; also adding menus to our add and update fragments: add_fragment_menu.xml and update_fragment_menu.xml; implementing Room into my ToDoApp, starting with Priority.kt, that is an enum class, and ToDoData.kt, that is the entity.

--- app/src/main/java/com/example/todoapp/fragment/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/fragment/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todoapp.fragment.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.todoapp.R
 
 
@@ -14,9 +12,13 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add, container, false)
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
--- app/src/main/java/com/example/todoapp/fragment/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragment/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todoapp.fragment.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -17,14 +15,22 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+        setUpListener(view)
+        setHasOptionsMenu(true)
+        return view
+    }
 
-        view.floatingActionButton.setOnClickListener{
+    private fun setUpListener(view: View) {
+        view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
-        view.listLayout.setOnClickListener{
+        view.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
-        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 }
